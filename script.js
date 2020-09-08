c937492c07501694f2ce393fa4c8fb085af3a542
Dylan Frank <drfrank22@gmail.com>
2020-09-08 00:26:33 -0400

    Added several prompts confirming length and confirmations of characters

--- script.js
+++ script.js
@@ -5,13 +5,30 @@ var specialChar = ["!", "@", "#", "$", "%", "&", "*", "?", "~", ".", "-", "_"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// var pLength = function () {
-//   return prompt ("How many characters would you like your password to include?");
-// };
+var confirmNumber;
+var confirmspecialChar;
+var confirmalphaLower;
+var confirmalphaUpper;
+
+// var pLength = ();
 // Write password to the #password input
 
 function generatePassword () {
-  var pLength = (prompt("How many characters would you like your password to include?")); 
+  var pLength = (prompt("How many characters would you like your password to include?"));
+
+  if (!pLength) {
+    alert ("This needs a value. Try Again.");
+  }
+  else if (pLength < 8 || pLength > 128) {
+    alert ("You must choose between 8 and 128 characters. Try Again.");
+  }
+  else {
+    confirmnumber = (confirm("Click ok if your password will contain numbers."));
+    confirmspecialChar = (confirm("Click ok if your password will contain special characters."));
+    confirmalphaLower = (confirm("Click ok if your password will contain lower case letters."));
+    confirmalphaUpper = (confirm("Click ok if your password will contain upper case letters."));
+  }
+
   };
 
 
